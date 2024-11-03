0c7e9e02a14b8cf9b73a210cabd321a83c11b17c
Prithesh Salian <pritheshmarodi@gmail.com>
2024-11-03 12:52:24 +0530

    Added static webpage for user endpoints(admin endpoints done)

--- src/app.js
+++ src/app.js
@@ -1,25 +1,37 @@
-import express from 'express'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-import { logoutUser } from './controllers/user.controller.js'
-const app = express()
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import { logoutUser } from './controllers/user.controller.js';
+import adminRouter from './routes/admin.routes.js';
+import studentRouter from './routes/student.routes.js';
+import subjectRouter from './routes/subject.routes.js';
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
-}))
+}));
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookieParser());
 
-import adminRouter from '../src/routes/admin.routes.js'
-import studentRouter from '../src/routes/student.routes.js'
-import subjectRouter from '../src/routes/subject.routes.js'
+app.use("/admin", adminRouter);
+app.use("/student", studentRouter);
+app.use("/subject", subjectRouter);
+app.use("/logout", logoutUser);
 
-app.use("/admin", adminRouter)
-app.use("/student", studentRouter)
-app.use("/subject", subjectRouter)
-app.use("/logout", logoutUser)
-export { app }
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+export { app };
